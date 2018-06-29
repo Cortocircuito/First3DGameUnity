46945794541d75d8df042b87d3355bf04d3cf37b
Cortocircuito <cortocircuit@gmail.com>
2018-06-30 00:14:13 +0200

    Initial commit

---
+++ Assets/Scripts/Pill.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pill : MonoBehaviour {
+
+    public static int pillCount;
+    public GameObject[] pills;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Pastilla " + gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("Player"))
+        {
+            pillCount++;
+            Debug.Log("Pastillas ingeridas " + pillCount);
+
+            Timer.AddTime();
+
+            pills = GameObject.FindGameObjectsWithTag("Pastilla");
+            int numberOfPills = pills.Length - 1;
+
+            if (numberOfPills == 0)
+            {
+                Debug.Log("Juego ha terminado");
+                GameObject gameManager = GameObject.Find("GameManager");
+
+                Destroy(gameManager);
+
+                GameObject fireworks = GameObject.FindGameObjectWithTag("Fireworks");
+                fireworks.GetComponent<ParticleSystem>().Play();
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public static float maxTime = 60f;
+
+    private static float countdown = 0f;
+    private GameObject[] pills;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Comienza el juego");
+        Pill.pillCount = 0;
+        countdown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pills = GameObject.FindGameObjectsWithTag("Pastilla");
+        string pillTotalMessage = string.Format("Pastillas pendiente ingerir {0}", pills.Length);
+        Debug.Log(pillTotalMessage);
+
+        //El deltaTime es el tiempo en segundos que ha pasado desde que se
+        //renderizó en pantalla el último frame anterior.
+
+        countdown -= Time.deltaTime; // countdown = countdown - Time.deltaTime;
+
+        //Debug.Log("Cuenta atrás :" + countdown);
+        if (countdown <= 0)
+        {
+            Debug.Log("Te has quedado sin tiempo... HAS PERDIDO!!!!");
+
+            Pill.pillCount = 0;
+
+            SceneManager.LoadScene("MainScene");
+        }
+        else
+        {
+            //Debug.Log("Tiempo " + countdown);
+        }
+    }
+
+    public static void AddTime(float addTime = 10)
+    {
+        countdown += addTime;
+        Debug.Log("Ha aumentado en " + addTime + " segundos más");
+    }
+}
